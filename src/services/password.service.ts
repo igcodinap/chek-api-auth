8d627e141c4ad0e69fcb7f0a42d4abd9e2c95261
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-01 19:34:28 -0400

    password implementation service

---
+++ src/services/password.service.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt';
+
+export class PasswordService {
+  static async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+  }
+
+  static async comparePasswords(password: string, hashedPassword: string): Promise<boolean> {
+    const isValid = await bcrypt.compare(password, hashedPassword);
+    return isValid;
+  }
+}
