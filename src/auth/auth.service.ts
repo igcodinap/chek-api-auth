d45ea8cfacf4286d23cf0a828e26fdaa26640236
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-01 20:20:59 -0400

    working on login logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,11 @@
-import bcrypt from 'bcrypt';
 import { IAuthRepository } from './auth.repository';
 import { User } from './user.model';
+import { PasswordService } from '../services/password.service';
+import { JwtService } from '../services/jwt.service';
 
 export interface IAuthService {
     login(email: string, password: string): Promise<User>;
 }
-
 export class AuthService implements IAuthService {
     repository: IAuthRepository;
     constructor(repo: IAuthRepository) {
@@ -15,13 +15,16 @@ export class AuthService implements IAuthService {
     async login(email: string, password: string) {
         try {
             const user = await this.repository.getByEmail(email);
+            console.log(user)
             if (!user) {
                 throw new Error('Email not found');
             }
-            const hashedPassword = bcrypt.hashSync(password, 10);
-            if (hashedPassword !== password) {
+            const isValid = PasswordService.comparePasswords(password, user.password);
+            if (!isValid) {
                 throw new Error('Invalid credentials');
             }
+            user.jwt_token = JwtService.generateToken({ user });
+            console.log(user.jwt_token, 'jwt_token')
             return user;
         } catch (error) {
             console.log(error)
--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,9 +1,10 @@
 import jwt from 'jsonwebtoken';
+import { User } from '../auth/user.model';
 
 const JWT_SECRET = 'irrelevant-secret-wip';
 
 export class JwtService {
-  static generateToken(payload: Record<string, number>): string {
+  static generateToken(payload: Record<string, User>): string {
     const token = jwt.sign(payload, JWT_SECRET);
     return token;
   }
