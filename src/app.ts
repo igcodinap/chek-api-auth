f8c13e35283ac532aba28bf85c6f855b4eba4620
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-08 16:53:14 -0400

    jest

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { ErrorMiddleware } from './errors/ErrorMiddleware';
 
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
 app.use(express.json());
 app.use('/auth', authRouter)
